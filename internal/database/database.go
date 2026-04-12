c5a55f2a25e2a26bcb5d568485e8de9ce72d0145
DusanStoiljkovic <dusanstoiljkovic02@gmail.com>
2026-04-12 22:52:18 +0200

    fix

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,8 +12,14 @@ import (
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := "host=db user=postgres password=postgres dbname=movie_management port=5432 ssl=disable"
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	name := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
 
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, pass, name, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB error: ", err)
